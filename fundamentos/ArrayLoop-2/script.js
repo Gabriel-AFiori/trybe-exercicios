ecf279dab727a030a8770bbd333ee6e9fff028af
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-02-28 15:44:42 -0300

    Exercicios de teste 1 e 2)

---
+++ fundamentos/ArrayLoop-2/script.js
@@ -0,0 +1,8 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+let menuServices = menu[1];
+menu.push('Contato')
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
