f408fc8a2c17fe523b5bd301eb9a379caa33f667
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-03-01 12:01:57 -0300

    Exercicios 1 - Todos os requisitos completos

--- fundamentos/ArrayLoop-12/script.js
+++ fundamentos/ArrayLoop-12/script.js
@@ -12,7 +12,7 @@ let arithmeticAverage = sum / numbers.length
 
 console.log(arithmeticAverage);
 
-if ( arithmeticAverage > 20) {
+if (arithmeticAverage > 20) {
     console.log('Valor maior que 20');
 }
 else if (arithmeticAverage <= 20) {
@@ -62,3 +62,28 @@ console.log(newNumbers);
 for (index = 0; index < newNumbers.length; index += 1) {
     console.log(newNumbers[index] / 2);
 }
+
+let word = 'tryber';
+let reverseWord = [];
+for (let index = 0; index < word.length; index += 1) {
+    reverseWord += word[word.length - 1 - index];
+}
+console.log(reverseWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggestWord = [];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+    if (biggestWord.length < array[index].length) {
+        biggestWord = array[index]
+    }
+}
+console.log(biggestWord);
+
+for (let index = 0; index < array.length; index += 1) {
+    if (smallestWord.length > array[index].length) {
+        smallestWord = array[index]
+    }
+}
+console.log(smallestWord);
