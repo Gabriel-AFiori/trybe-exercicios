4fe941f436cadde83db7d1dd99ffa30ef29d8f89
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-03-01 02:10:27 -0300

    Exercicios 1 - 9 requisitos completos

--- fundamentos/ArrayLoop-12/script.js
+++ fundamentos/ArrayLoop-12/script.js
@@ -32,11 +32,33 @@ for (let index = 0; index < numbers.length; index += 1) {
 console.log(highNumber);
 
 let oddNumber = [];
-
 for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] % 2 == 1) {
         oddNumber.push(numbers[index])
     }
+    else {
+        console.log('Nenhum valor ímpar encontrado');
+    }
 }
 
 console.log(oddNumber);
+
+let lowerNumber = numbers[0];
+for (let index = 0; index < numbers.length; index += 1) {
+    if (lowerNumber > numbers[index]) {
+        lowerNumber = numbers[index]
+    }
+}
+
+console.log(lowerNumber);
+
+let newNumbers = [];
+for (let index = 1; index <= 25; index += 1) {
+    newNumbers.push(index);
+}
+
+console.log(newNumbers);
+
+for (index = 0; index < newNumbers.length; index += 1) {
+    console.log(newNumbers[index] / 2);
+}
